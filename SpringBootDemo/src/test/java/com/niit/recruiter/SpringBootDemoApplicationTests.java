ea17fd67f1c98937ba4e9c7a573221771181fc2e
root <root@deep-Lenovo-G50-45>
2020-03-31 15:28:49 +0530

    Initial Spring BOOT

---
+++ SpringBootDemo/src/test/java/com/niit/recruiter/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.recruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
